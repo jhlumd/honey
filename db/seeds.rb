d5176681a2a7a79b58fca633c8dcf8595499b16c
Jaehyuk Lee <jhlumd@gmail.com>
2020-08-21 22:09:14 -0700

    Add prelim seeds for Combs and Bees to visualize models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Combs and Bees
+Comb.destroy_all
+WorkerBee.destroy_all
+
+num_combs = 5   # Set number of Combs
+ss_min = 10.0   # Set min p/g SweetSpot
+ss_max = 15.0   # Set max p/g SweetSpot
+pop_min = 10    # Set min number of Bees in a Comb
+pop_max = 20    # Set max number of Bees in a Comb
+
+num_combs.times do
+  supervisor = Faker::Name.first_name
+  sweet_spot = rand(ss_min..ss_max).round(1)
+  comb1 = Comb.create(supervisor: supervisor, sweet_spot: sweet_spot)
+
+  population = rand(pop_min..pop_max)
+
+  population.times do
+    name = Faker::Name.first_name
+    WorkerBee.create(name: name, comb_id: comb1.id)
+  end
+end
