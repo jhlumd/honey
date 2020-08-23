d927328fc6bd2a813930da543ab0d2a3fad6d9b9
Jaehyuk Lee <jhlumd@gmail.com>
2020-08-23 00:51:37 -0700

    Change route to remove nesting update appointments

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   get 'worker_bees/index'
 
   root to: 'worker_bees#index'
-  resources :worker_bees, only: [:index, :show, :update] do
-    resources :appointments, only: [:update]
-  end
+  resources :worker_bees, only: [:index, :show, :update]
+  resources :appointments, only: [:update]
 end
