7dd3ab72a21da744a12fd7ed7a6804f675264d51
Jaehyuk Lee <jhlumd@gmail.com>
2020-08-25 00:06:59 -0700

    Import basic two-axes combo graph template

--- app/assets/javascripts/draw_graph.js
+++ app/assets/javascripts/draw_graph.js
@@ -1,40 +1,123 @@
 function drawGraph() {
   Highcharts.chart("graph-container", {
     chart: {
-      type: "bar",
+      zoomType: "xy",
     },
     title: {
-      text: "Stacked bar chart",
+      text: "Average Monthly Temperature and Rainfall in Tokyo",
     },
-    xAxis: {
-      categories: ["Apples", "Oranges", "Pears", "Grapes", "Bananas"],
+    subtitle: {
+      text: "Source: WorldClimate.com",
     },
-    yAxis: {
-      min: 0,
-      title: {
-        text: "Total fruit consumption",
+    xAxis: [
+      {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec",
+        ],
+        crosshair: true,
       },
+    ],
+    yAxis: [
+      {
+        // Primary yAxis
+        labels: {
+          format: "{value}°C",
+          style: {
+            color: Highcharts.getOptions().colors[1],
+          },
+        },
+        title: {
+          text: "Temperature",
+          style: {
+            color: Highcharts.getOptions().colors[1],
+          },
+        },
+      },
+      {
+        // Secondary yAxis
+        title: {
+          text: "Rainfall",
+          style: {
+            color: Highcharts.getOptions().colors[0],
+          },
+        },
+        labels: {
+          format: "{value} mm",
+          style: {
+            color: Highcharts.getOptions().colors[0],
+          },
+        },
+        opposite: true,
+      },
+    ],
+    tooltip: {
+      shared: true,
     },
     legend: {
-      reversed: true,
-    },
-    plotOptions: {
-      series: {
-        stacking: "normal",
-      },
+      layout: "vertical",
+      align: "left",
+      x: 120,
+      verticalAlign: "top",
+      y: 100,
+      floating: true,
+      backgroundColor:
+        Highcharts.defaultOptions.legend.backgroundColor || // theme
+        "rgba(255,255,255,0.25)",
     },
     series: [
       {
-        name: "John",
-        data: [5, 3, 4, 7, 2],
+        name: "Rainfall",
+        type: "column",
+        yAxis: 1,
+        data: [
+          49.9,
+          71.5,
+          106.4,
+          129.2,
+          144.0,
+          176.0,
+          135.6,
+          148.5,
+          216.4,
+          194.1,
+          95.6,
+          54.4,
+        ],
+        tooltip: {
+          valueSuffix: " mm",
+        },
       },
       {
-        name: "Jane",
-        data: [2, 2, 3, 2, 1],
-      },
-      {
-        name: "Joe",
-        data: [3, 4, 4, 2, 5],
+        name: "Temperature",
+        type: "spline",
+        data: [
+          7.0,
+          6.9,
+          9.5,
+          14.5,
+          18.2,
+          21.5,
+          25.2,
+          26.5,
+          23.3,
+          18.3,
+          13.9,
+          9.6,
+        ],
+        tooltip: {
+          valueSuffix: "°C",
+        },
       },
     ],
   });
